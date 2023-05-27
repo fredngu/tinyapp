636c08e3569d4027dab331ef1e44beba080cc3e9
Frederik Nguyen <frederik.nguyen@outlook.com>
2023-05-27 18:14:36 -0400

    Add logout feature

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+const cookieParser = require("cookie-parser");
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -16,6 +17,7 @@ const urlDatabase = {
 };
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // Homepage ('/')
 app.get("/", (req, res) => {
@@ -32,24 +34,30 @@ app.get("/urls.json", (req, res) => {
 
 // URL Main Page
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { username: req.cookies["username"], urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 // URL Shortening Page
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] }
+  res.render("urls_new", templateVars);
 });
 
 // URL Page for the Short URLS
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  const templateVars = { username: req.cookies["username"], id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
 
 app.post("/login", (req, res) => {
   console.log(req.body.username)
   res.cookie('username', req.body.username);
+  res.redirect('/urls');
+})
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
   res.redirect('/urls');
 })
 
